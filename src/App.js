14286c9f29ab44cd0ea7da070c227f8d1e6c7df7
Jvshen <justin2003.shen@gmail.com>
2025-04-09 21:03:01 -0700

    add import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { createWorker } from 'tesseract.js';
 
 /* ----------  TESSERACT WORKER CONFIG  ---------- */
